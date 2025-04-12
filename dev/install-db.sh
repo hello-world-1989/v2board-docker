9174df03eba2857d1d8f994aef1738a065d1c3e6
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2025-04-12 04:41:11 +0000

    wip dev

---
+++ dev/install-db.sh
@@ -0,0 +1,27 @@
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+yes '' | sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/dc
+sudo chmod +x /usr/local/bin/dc
+
+sudo apt install unzip -y
+
+mkdir end-gfw
+cd end-gfw
+
+
+wget https://github.com/hello-world-1989/v2board-docker/raw/master/dev/docker-compose.yml
